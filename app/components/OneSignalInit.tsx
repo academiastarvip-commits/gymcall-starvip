1b4c80fd30ded552171a6c26d55e0e641ab5fb50
nicolly <academiastarvip@gmail.com>
2026-07-20 10:20:49 -0300

    Corrige inicialização do OneSignal

--- app/components/OneSignalInit.tsx
+++ app/components/OneSignalInit.tsx
@@ -5,23 +5,16 @@ import OneSignal from "react-onesignal";
 
 export default function OneSignalInit() {
   useEffect(() => {
-    console.log("🚀 OneSignalInit foi carregado");
-
     async function init() {
       try {
-        console.log("Iniciando OneSignal...");
-
         await OneSignal.init({
           appId: "99d5bf74-03ca-452d-b503-4cd50b25de0c",
-          notifyButton: {
-            enable: false,
-          },
           allowLocalhostAsSecureOrigin: true,
         });
 
         console.log("✅ OneSignal iniciado");
       } catch (error) {
-        console.error("❌ Erro ao iniciar OneSignal:", error);
+        console.error("Erro ao iniciar OneSignal:", error);
       }
     }
 
